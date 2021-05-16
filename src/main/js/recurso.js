faadb34ca1d5a3e45c20c9f6ddcca497535bec84
Everaldo-Gomes <everaldo332@gmail.com>
2021-05-16 08:38:38 -0300

    update

--- src/main/js/recurso.js
+++ src/main/js/recurso.js
@@ -1,13 +1,19 @@
 "use strict"
 
 var transferResourceSpace = document.querySelector(".TransferirRecurso");
-
+var qntColumn = document.querySelector(".quantityColumn");
 
 function compartilharRecurso() {
-    transferResourceSpace.classList.remove("hidden");  
+    transferResourceSpace.classList.remove("hidden");
+    qntColumn.classList.remove('hidden');
 }
 
 function trocarResponsavel() {
     transferResourceSpace.classList.remove("hidden");
+    qntColumn.classList.add('hidden');
+}
+
+function transferConfirmation() {
+    
 }
 
