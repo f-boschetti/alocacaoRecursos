c8dd9f26ffbf20186a207aeb6b92d737cbc121ca
Everaldo-Gomes <everaldo332@gmail.com>
2021-05-15 21:55:09 -0300

    novas funções

---
+++ src/main/js/recurso.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var transferResourceSpace = document.querySelector(".TransferirRecurso");
+
+
+function compartilharRecurso() {
+    transferResourceSpace.classList.remove("hidden");  
+}
+
+function trocarResponsavel() {
+    transferResourceSpace.classList.remove("hidden");
+}
+
