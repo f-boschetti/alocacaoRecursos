bf829701c642e820e3f40dd989ff76723bb15afe
felipe_Boschetti <felipeboss232@gmail.com>
2021-05-22 18:28:54 -0300

    Adicionado links para ter acesso as paginas html

--- src/main/java/br/edu/uffs/engSoftware/enums/Categoria.java
+++ src/main/java/br/edu/uffs/engSoftware/enums/Categoria.java
@@ -1,13 +1,14 @@
 package br.edu.uffs.engSoftware.enums;
 
 /**
- * D
  * @author felip
- * como podem ver, não entendi
+ * 
  */
 
 public enum Categoria {
 
-	NAO, ENTENDI, OQ, ISSO, SIGNIFICA, DIREITO, PRAMIM, SERIA, TIPO, SALA, OBJETO, QUEBRAVEL, ETC
+	MATERIAL,
+	SALA,
+	ELETRONICO
 
 }
